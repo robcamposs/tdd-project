c12ab219acb5480914b438740c56d73c3e795b5e
robcamposs <roberta.mpcampos@gmail.com>
2026-04-12 23:11:31 -0300

    Refatorando o Teste funcional

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
@@ -11,18 +12,23 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # Método auxiliar para evitar repetição de código
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self): 
-    
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a entrar com um item TODO imediatamente
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')  
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # Ela digita "Estudar testes funcionais" em uma caixa de texto
@@ -32,28 +38,18 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')  
         inputbox.send_keys('Estudar testes de unidade')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
-        self.assertIn('2: Estudar testes de unidade', [row.text for row in rows])
-
-        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
-
-        # Ela visita a URL: a sua lista TODO ainda está armazenada
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.check_for_row_in_list_table('2: Estudar testes de unidade')
 
         # Satisfeita, ela vai dormir
 
